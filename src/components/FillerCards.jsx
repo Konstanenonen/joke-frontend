1cc0d94544195b5ad4a08c7e86e9276f42264083
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-27 23:44:20 +0300

    Made the loading more obvious with ...

--- src/components/FillerCards.jsx
+++ src/components/FillerCards.jsx
@@ -4,14 +4,14 @@ import JokeCard from './JokeCard';
 function FillerCards() {
   return (
     <>
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
-      <JokeCard title="Loading" category="Loading" body="Loading" />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
+      <JokeCard title="Loading..." category="Loading..." body="Loading..." />
     </>
   );
 }
