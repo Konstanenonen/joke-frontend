81580fb1b58a00094aa53930b8e3a7d9bb59647c
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-01 23:53:52 +0300

    Added color highlighting to the filter that is active

--- src/components/TagFilters.jsx
+++ src/components/TagFilters.jsx
@@ -1,23 +1,51 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 function TagFilters(props) {
   const { setCategory } = props;
+  const [activeFilter, setActiveFilter] = useState(null);
+
+  function handleClick(category) {
+    setCategory(category);
+    setActiveFilter(category);
+  }
+
+  const styles = {
+    color: 'white',
+    backgroundColor: 'green',
+    border: '1px solid black',
+  };
 
   return (
     <div className="filter-container">
       <h2>Select Category</h2>
       <div className="button-container">
-        <button onClick={() => setCategory(null)} type="button">
+        <button
+          onClick={() => handleClick(null)}
+          type="button"
+          style={activeFilter === null ? styles : null}
+        >
           All Jokes
         </button>
-        <button onClick={() => setCategory('Dad Joke')} type="button">
+        <button
+          onClick={() => handleClick('Dad Joke')}
+          type="button"
+          style={activeFilter === 'Dad Joke' ? styles : null}
+        >
           Dad Jokes
         </button>
-        <button onClick={() => setCategory('Dark Humor')} type="button">
+        <button
+          onClick={() => handleClick('Dark Humor')}
+          type="button"
+          style={activeFilter === 'Dark Humor' ? styles : null}
+        >
           Dark Humor
         </button>
-        <button onClick={() => setCategory('Knock-Knock Joke')} type="button">
+        <button
+          onClick={() => handleClick('Knock-Knock Joke')}
+          type="button"
+          style={activeFilter === 'Knock-Knock Joke' ? styles : null}
+        >
           Knock-Knock Jokes
         </button>
       </div>
