4ce639de7f89986de6a31285149d1f13cb3b79c3
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-03 22:54:01 +0300

    Added icons to font button

--- src/components/FontButton.jsx
+++ src/components/FontButton.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react';
 import { FontContext } from '../Context';
+import boring from '../images/boring.png';
+import fun from '../images/fun.png';
 
 function FontButton(props) {
   const { setBoringFont } = props;
@@ -11,9 +13,15 @@ function FontButton(props) {
   return (
     <button
       style={font}
+      className="theme-button"
       type="button"
       onClick={() => setBoringFont((prevState) => !prevState)}
     >
+      {boringFont ? (
+        <img height={20} src={fun} alt="A happy face" />
+      ) : (
+        <img height={20} src={boring} alt="A sad face" />
+      )}
       {boringFont ? 'Fun font' : 'Boring font'}
     </button>
   );
